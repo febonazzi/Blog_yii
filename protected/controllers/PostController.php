3eb473ffa925052edf431c0d622ae789dcfc199f
febonazzi <wowmonec@gmail.com>
2015-04-30 18:17:24 +0500

    Post view

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -46,11 +46,13 @@ class PostController extends Controller
 	 */
 	public function actionView($id)
 	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
+		$post=$this->loadModel();
+	    $this->render('view',array(
+	        'model'=>$post,
+	    ));
 	}
 
+	private $_model;
 	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
@@ -117,10 +119,24 @@ class PostController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Post');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
+		$criteria=new CDbCriteria(array(
+        'condition'=>'status='.Post::STATUS_PUBLISHED,
+        'order'=>'update_time DESC',
+        'with'=>'commentCount',
+	    ));
+	    if(isset($_GET['tag']))
+	        $criteria->addSearchCondition('tags',$_GET['tag']);
+	 
+	    $dataProvider=new CActiveDataProvider('Post', array(
+	        'pagination'=>array(
+	            'pageSize'=>5,
+	        ),
+	        'criteria'=>$criteria,
+	    ));
+	 
+	    $this->render('index',array(
+	        'dataProvider'=>$dataProvider,
+	    ));
 	}
 
 	/**
@@ -147,10 +163,21 @@ class PostController extends Controller
 	 */
 	public function loadModel($id)
 	{
-		$model=Post::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
+		if($this->_model===null)
+	    {
+	        if(isset($_GET['id']))
+	        {
+	            if(Yii::app()->user->isGuest)
+	                $condition='status='.Post::STATUS_PUBLISHED
+	                    .' OR status='.Post::STATUS_ARCHIVED;
+	            else
+	                $condition='';
+	            $this->_model=Post::model()->findByPk($_GET['id'], $condition);
+	        }
+	        if($this->_model===null)
+	            throw new CHttpException(404,'Запрашиваемая страница не существует.');
+	    }
+	    return $this->_model;
 	}
 
 	/**
